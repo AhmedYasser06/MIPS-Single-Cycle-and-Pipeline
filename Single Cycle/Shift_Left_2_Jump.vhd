2d66855fa1d25f6a314a97cc6d1f6d2b8a13fb42
Ahmed Yasser <154678540+AhmedYasser06@users.noreply.github.com>
2025-12-17 18:19:03 +0200

    Update Shift_Left_2_Jump.vhd

--- Single Cycle/Shift_Left_2_Jump.vhd
+++ Single Cycle/Shift_Left_2_Jump.vhd
@@ -1,9 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- Shift Left 2 unit for Jump address
--- Multiplies the 26-bit instruction field by 4
-
 entity JumpShiftLeft2 is
     port(
         jump_instr : in  std_logic_vector(25 downto 0);  -- instruction[25:0]
