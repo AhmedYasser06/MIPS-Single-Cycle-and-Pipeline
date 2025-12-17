3144b0c8f417b595cd1721874cceb230f83718f1
Ahmed Yasser <154678540+AhmedYasser06@users.noreply.github.com>
2025-12-17 18:19:15 +0200

    Update Shift_Left_2.vhd

--- Single Cycle/Shift_Left_2.vhd
+++ Single Cycle/Shift_Left_2.vhd
@@ -2,9 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Shift Left 2 unit
--- Multiplies the input by 4 (used for branch address calculation)
-
 entity ShiftLeft2 is
     port(
         addr_in  : in  std_logic_vector(31 downto 0);
